d405b5e6d9b551e2cb0e59960c178df52619f6d4
markxwell-strong <cleandemon5@gmail.com>
2024-11-07 17:16:34 +0300

    correct quiz 2

--- CAT2_Q2.c
+++ CAT2_Q2.c
@@ -4,20 +4,32 @@ ADM:CT101/G/22763/24
 Date of coding: 6/11/2024*/
 #include <stdio.h>
 int main() {
-    int scores[4][2] = {
+    int scores1[2][2] = {
         {65, 92},
-        {84, 72},
+        {84, 72}
+    };
+    int scores2[2][2] = {
         {35, 70},
         {59, 67}
     };
-    printf("Scores:\n");
-    for (int i = 0; i < 4; i++)
+    printf("SCORES\n");
+    for (int i = 0; i < 2; i++)
     {
         for (int j = 0; j < 2; j++)
         {
-            printf("%d", scores[i][j]);
+            printf("%d", scores1[i][j]);
         }
-        print("\n");
+        printf("\n");
     }
+    printf("\n \n \n");//This is to separate the scores
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            printf("%d", scores2[i][j]);
+        }
+        printf("\n");
+    }
+    
     return 0;
 }
